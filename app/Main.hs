9b69969f0e832e0ae077f5c860f354c66dbbc290
JekaGrib <1234jane@mail.ru>
2020-08-02 00:47:47 +0300

    add get-create-deleteUser

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,17 @@
 
 module Main where
 
-import Network.Wai
-import Network.HTTP.Types (status200, status404)
-import Network.Wai.Handler.Warp (run)
-import Data.Aeson
-import Data.Text
-import Data.ByteString.Builder (lazyByteString)
-import Database.PostgreSQL.Simple
+import           Network.HTTP.Simple            ( parseRequest, setRequestBody, getResponseBody, httpLBS )
+import           Network.Wai
+import           Network.HTTP.Types             ( status200, status404, status301, movedPermanently301 )
+import           Network.HTTP.Types.URI         ( queryToQueryText )
+import           Network.Wai.Handler.Warp       ( run )
+import           Data.Aeson
+import           Data.Text
+import           Data.ByteString.Builder        ( lazyByteString )
+import           Database.PostgreSQL.Simple
+import           Network.HTTP.Simple            ( parseRequest, setRequestBody, getResponseBody, httpLBS )
+import           Data.Maybe                     ( fromJust )
 
 createAuthorsTable :: IO ()
 createAuthorsTable = do
@@ -89,9 +93,57 @@ createDbStructure = do
   createDraftsPicsTable
   createDraftsTagsTable
 
+data CreateUserResponse = CreateUserResponse {
+      user_id      :: Integer
+    , first_name   :: Text
+    , last_name    :: Text
+    , user_pic_id  :: Integer
+    , user_pic_url :: Text
+    } deriving Show
+
+data DeleteUserResponse = DeleteUserResponse {ok :: Bool}
+
+instance ToJSON CreateUserResponse where
+    toJSON (CreateUserResponse user_id first_name last_name user_pic_id user_pic_url) =
+        object ["user_id" .= user_id, "first_name" .= first_name, "last_name" .= last_name, "user_pic_id" .= user_pic_id, "user_pic_url" .= user_pic_url]
+    toEncoding (CreateUserResponse user_id first_name last_name user_pic_id user_pic_url) =
+        pairs ("user_id" .= user_id <> "first_name" .= first_name <> "last_name" .= last_name <> "user_pic_id" .= user_pic_id <> "user_pic_url" .= user_pic_url)
+
+instance ToJSON DeleteUserResponse where
+    toJSON (DeleteUserResponse ok) =
+        object ["ok" .= ok]
+    toEncoding (DeleteUserResponse ok) =
+        pairs ("ok" .= ok )
+
+application req send = do
+  let okHelper = send . responseBuilder status200 [("Content-Type", "application/json; charset=utf-8")]
+  case pathInfo req of
+    [createUser]        -> do
+      let passwordParam   = fromJust . fromJust . lookup   "first_name" $ queryToQueryText $ queryString req
+      let firstNameParam  = fromJust . fromJust . lookup   "first_name" $ queryToQueryText $ queryString req
+      let lastNameParam   = fromJust . fromJust . lookup    "last_name" $ queryToQueryText $ queryString req
+      let userPicUrlParam = fromJust . fromJust . lookup "user_pic_url" $ queryToQueryText $ queryString req
+      conn <- connectPostgreSQL "host='localhost' port=5432 user='evgenya' dbname='newdb' password='123456'"
+      [Only picId]  <- query conn "INSERT INTO pics ( pic_url ) VALUES ( ? ) RETURNING pic_id" [userPicUrlParam]  
+      [Only userId] <- query conn "INSERT INTO users ( password, first_name , last_name , user_pic_id , user_create_date, admin) VALUES ( ?,?,?,?, '20100103', false ) RETURNING user_id" [ passwordParam , firstNameParam, lastNameParam, pack (show picId) ]
+      okHelper $ lazyByteString $ encode (CreateUserResponse {user_id = userId , first_name = firstNameParam , last_name = lastNameParam, user_pic_id = picId, user_pic_url = pack ("http://localhost:3000/pic_" ++ show picId)})
+    ["getUser", _]      -> do
+      let userId = (pathInfo req) !! 1
+      conn <- connectPostgreSQL "host='localhost' port=5432 user='evgenya' dbname='newdb' password='123456'"
+      [Only firstName] <- query conn "SELECT first_name FROM users WHERE user_id = ? " [userId ]
+      [Only lastName] <- query conn "SELECT last_name FROM users WHERE user_id = ? " [userId]
+      [Only picId] <- query conn "SELECT user_pic_id FROM users WHERE user_id = ? " [userId]
+      okHelper $ lazyByteString $ encode (CreateUserResponse {user_id = read (unpack(userId)) , first_name = firstName , last_name = lastName, user_pic_id = picId, user_pic_url = pack ("http://localhost:3000/pic_" ++ show picId)})
+    ["deleteUser", _]   -> do
+      let userId = (pathInfo req) !! 1
+      conn <- connectPostgreSQL "host='localhost' port=5432 user='evgenya' dbname='newdb' password='123456'"
+      execute conn "DELETE FROM users WHERE user_id = ? " [userId]
+      okHelper $ lazyByteString $ encode (DeleteUserResponse {ok = True})
+      
 main :: IO ()
 main = do
   createDbStructure
+  run 3000 application
 
 
 
