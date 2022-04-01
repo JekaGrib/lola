a7e821d937b241f230bec0aef0d44b4cacc5eba2
JekaGrib <1234jane@mail.ru>
2022-04-01 16:28:16 +0300

    add tests

--- test/Spec/Author.hs
+++ test/Spec/Author.hs
@@ -75,89 +75,60 @@ testAuthor = hspec $ do
       eitherResp <- evalStateT (runExceptT $ deleteAuthor handle 4) []
       eitherResp
         `shouldBe` (Right $ ResponseInfo status204 [textHeader] "Status 204 No data")    
-{-}  describe "workWithCats (ToPost)" $ do
+  describe "workWithAuthors (ToPost)" $ do
     it "work with valid DB answer, without super category" $ do
-      state <- execStateT (runExceptT $ workWithCats handle qStr2 ToPost) []
-      reverse state
-        `shouldBe` [AuthMock (SelectTokenKeyForUser 152),CatMock (InsertReturnCat "dogs")]
-      eitherResp <- evalStateT (runExceptT $ workWithCats handle qStr2 ToPost) []
-      eitherResp
-        `shouldBe` (Right $ ResponseInfo status201 [textHeader, ("Location", "http://localhost:3000/categories/14")] "Status 201 Created")
-    it "work with valid DB answer, with super category" $ do
-      state <- execStateT (runExceptT $ workWithCats handle qStr3 ToPost) []
+      state <- execStateT (runExceptT $ workWithAuthors handle qStr2 ToPost) []
       reverse state
         `shouldBe` 
         [ AuthMock (SelectTokenKeyForUser 152)
-        , ExistMock (IsExist (CategoryId 2))
-        , CatMock (InsertReturnSubCat "dogs" 2)
+        , ExistMock (IsExist (UserId 3))
+        , AuthorMock (IsUserAuthor 3)
+        , AuthorMock (InsertReturnAuthor 3 "author")
         ]
-      eitherResp <- evalStateT (runExceptT $ workWithCats handle qStr3 ToPost) []
+      eitherResp <- evalStateT (runExceptT $ workWithAuthors handle qStr2 ToPost) []
       eitherResp
-        `shouldBe` (Right $ ResponseInfo status201 [textHeader, ("Location", "http://localhost:3000/categories/14")] "Status 201 Created")
-  describe "workWithCats (ToGet)" $ do
+        `shouldBe` (Right $ ResponseInfo status201 [textHeader, ("Location", "http://localhost:3000/authors/14")] "Status 201 Created")
+  describe "workWithAuthors (ToGet)" $ do
     it "work with valid DB answer" $ do
-      state <- execStateT (runExceptT $ workWithCats handle [] (ToGet 4)) []
-      reverse state
-        `shouldBe` 
-        [ ExistMock (IsExist (CategoryId 4))
-        , MakeCatRMock (SelectCats    4)
-        , MakeCatRMock (SelectSubCats 4)
-        , MakeCatRMock (SelectCats    1)
-        , MakeCatRMock (SelectSubCats 1)
-        ]
-      eitherResp <- evalStateT (runExceptT $ workWithCats handle [] (ToGet 4)) []
-      eitherResp
-        `shouldBe` (Right $ ResponseInfo status200 [jsonHeader] (encode $ SubCatResponse 4 "d" [11,12] $ CatResponse 1 "a" [4,5,6]))
-  describe "workWithCats (ToPut)" $ do
-    it "work with valid DB answer, without super category" $ do
-      state <- execStateT (runExceptT $ workWithCats handle qStr2 (ToPut 4)) []
-      reverse state
-        `shouldBe` 
-        [ AuthMock     (SelectTokenKeyForUser 152)
-        , ExistMock    (IsExist (CategoryId 4))
-        , CatMock      (UpdateDbCat "dogs" 4)
-        , MakeCatRMock (SelectCats    4)
-        , MakeCatRMock (SelectSubCats 4)
-        , MakeCatRMock (SelectCats    1)
-        , MakeCatRMock (SelectSubCats 1)
-        ]
-    it "work with valid DB answer, with super category" $ do
-      state <- execStateT (runExceptT $ workWithCats handle qStr3 (ToPut 4)) []
+      state <- execStateT (runExceptT $ workWithAuthors handle qStr1 (ToGet 4)) []
       reverse state
         `shouldBe` 
         [ AuthMock (SelectTokenKeyForUser 152)
-        , ExistMock (IsExist (CategoryId 4))
-        , ExistMock (IsExist (CategoryId 2))
-        , MakeCatRMock (SelectSubCats 4)
-        , MakeCatRMock (SelectSubCats 11)
-        , MakeCatRMock (SelectSubCats 12)
-        , MakeCatRMock (SelectSubCats 16)
-        , MakeCatRMock (SelectSubCats 17)
-        , CatMock      (UpdateDbSubCat "dogs" 2 4)
-        , MakeCatRMock (SelectCats    4)
-        , MakeCatRMock (SelectSubCats 4)
-        , MakeCatRMock (SelectCats    1)
-        , MakeCatRMock (SelectSubCats 1)
+        , ExistMock (IsExist (AuthorId 4))
+        , AuthorMock (SelectAuthors 4)
         ]
-  describe "workWithCats (ToDelete)" $ do
+      eitherResp <- evalStateT (runExceptT $ workWithAuthors handle qStr1 (ToGet 4)) []
+      eitherResp
+        `shouldBe` (Right $ ResponseInfo status200 [jsonHeader] $ encode $ 
+          AuthorResponse 4 "author" 3)
+  describe "workWithAuthors (ToPut)" $ do
     it "work with valid DB answer" $ do
-      state <- execStateT (runExceptT $ workWithCats handle qStr1 (ToDelete 4)) []
+      state <- execStateT (runExceptT $ workWithAuthors handle qStr2 (ToPut 4)) []
       reverse state
         `shouldBe` 
-        [ AuthMock     (SelectTokenKeyForUser 152)
-        , ExistMock    (IsExist (CategoryId 4))
-        , MakeCatRMock (SelectSubCats 4)
-        , MakeCatRMock (SelectSubCats 11)
-        , MakeCatRMock (SelectSubCats 12)
-        , MakeCatRMock (SelectSubCats 16)
-        , MakeCatRMock (SelectSubCats 17)
+        [ AuthMock (SelectTokenKeyForUser 152)
+        , ExistMock (IsExist (AuthorId 4))
+        , ExistMock (IsExist (UserId 3))
+        , AuthorMock (SelectAuthorsForUser 3)
+        , AuthorMock (UpdateDbAuthor 3 "author" 4)
+        ]
+  describe "workWithAuthors (ToDelete)" $ do
+    it "work with valid DB answer" $ do
+      state <- execStateT (runExceptT $ workWithAuthors handle qStr1 (ToDelete 4)) []
+      reverse state
+        `shouldBe` 
+        [ AuthMock (SelectTokenKeyForUser 152)
+        , ExistMock (IsExist (AuthorId 4))
+        , AuthorMock (SelectDraftsForAuthor 4)
         , TRANSACTIONOPEN
-        , CatMock (UpdateDbCatsForPosts  1 [4,11,12,16,17])
-        , CatMock (UpdateDbCatsForDrafts 1 [4,11,12,16,17])
-        , CatMock (DeleteDbCats [4,11,12,16,17])
+        , AuthorMock (UpdateDbAuthorForPosts 1 4)
+        , DeleteManyMock (DeleteDbPicsForDrafts [2,5])
+        , DeleteManyMock (DeleteDbTagsForDrafts [2,5])
+        , DeleteManyMock (DeleteDbDrafts [2,5])
+        , AuthorMock (DeleteDbAuthor 4)
         , TRANSACTIONCLOSE
         ]
-      eitherResp <- evalStateT (runExceptT $ workWithCats handle qStr1 (ToDelete 4)) []
+      eitherResp <- evalStateT (runExceptT $ workWithAuthors handle qStr1 (ToDelete 4)) []
       eitherResp
         `shouldBe` (Right $ ResponseInfo status204 [textHeader] "Status 204 No data")   
--}
+
--- test/Spec/Author/QStrExample.hs
+++ test/Spec/Author/QStrExample.hs
@@ -10,7 +10,7 @@ qStr1 :: QueryText
 qStr1 = [("token", Just "152.hij.83a6f25e7d42750b6b1769efa231cccbfd8d319c")]
 
 qStr2 :: QueryText
-qStr2 = [("category_name", Just "dogs"), ("token", Just "152.hij.83a6f25e7d42750b6b1769efa231cccbfd8d319c")]
+qStr2 = [("author_info", Just "author"), ("user_id", Just "3"), ("token", Just "152.hij.83a6f25e7d42750b6b1769efa231cccbfd8d319c")]
 
 qStr3 :: QueryText
 qStr3 = [("category_name", Just "dogs"), ("super_category_id", Just "2"), ("token", Just "152.hij.83a6f25e7d42750b6b1769efa231cccbfd8d319c")]
